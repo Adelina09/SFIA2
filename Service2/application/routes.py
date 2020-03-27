000fdd8be7bb8cf72642ad721e67458dd2cda693
Adelina09 <adebr40@gmail.com>
2020-03-27 09:27:32 +0000

    story generator

--- Service2/application/routes.py
+++ Service2/application/routes.py
@@ -2,9 +2,9 @@ from application import app
 import random
 
 
-@app.route('/randomphrase', methods=['GET'])
+@app.route('/randomstory', methods=['GET'])
 def beginning():
 
-	list = ['You are a','Hey you','You want to','Sup my','How are','You happy','Can you']
+	list = ["Once upon a time, a prince met a princess. ","It is now or never! the prince shouted to his trusted dragon. ","It all started when she was born. ","All the decisions they've ever made are coming back to them like a dream. ","Not so tough now! Let's go Dunbar! ","And so it begins. ","The story begins with their disappearance. "]
 	
 	return list[random.randrange(6)]
--- Service4/application/routes.py
+++ Service4/application/routes.py
@@ -4,7 +4,7 @@ import requests
 
 @app.route('/randomword', methods=['GET'])
 def sentence():
-    beginning = requests.get('http://localhost:5001/randomphrase')
-    ending = requests.get('http://localhost:5002/randomphrase')
+    beginning = requests.get('http://localhost:5001/randomstory')
+    ending = requests.get('http://localhost:5002/randomstory')
     response = beginning.text + " " + ending.text
     return response
